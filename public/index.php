cf93f480b664498fa37188bad7078e1ee527838f
Shahen <s.torosyan83@gmail.com>
2021-12-09 15:26:45 +0400

    implemented post route

--- public/index.php
+++ public/index.php
@@ -7,7 +7,9 @@ use app\core\Application;
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'home');
-
 $app->router->get('/contact', 'contact');
+$app->router->post('/contact', function() {
+    return "handling submitted data";
+});
 
 $app->run();
